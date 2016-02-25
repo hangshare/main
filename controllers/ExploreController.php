28cb3ae24dfb398f6e99290124ca0af49599891e
kaldmax <kaldmax@hotmail.com>
2016-02-24 16:18:09 -0800

    SIMPLE CODE

--- controllers/ExploreController.php
+++ controllers/ExploreController.php
@@ -259,7 +259,7 @@ class ExploreController extends Controller {
         $pageSize = 14;
         $query = Post::find();
         $query->orderBy('created_at DESC');
-        $query->joinWith(['postTags', 'postTags.tags', 'user']);
+        $query->joinWith(['postTags', 'postTags.tags', 'user', 'PostBodies']);
         $query->where('post.type = 0 AND post.deleted=0');
         $query->andWhere(['<>', 'cover', '']);
         if (!empty($tag)) {
