ac32f3358eef8b5fc363a466121ac96d82a52852
kaldmax <kaldmax@hotmail.com>
2016-02-24 16:16:40 -0800

    SIMPLE CODE

--- controllers/ExploreController.php
+++ controllers/ExploreController.php
@@ -259,10 +259,10 @@ class ExploreController extends Controller {
         $pageSize = 14;
         $query = Post::find();
         $query->orderBy('created_at DESC');
+        $query->joinWith(['postTags', 'postTags.tags', 'user']);
         $query->where('post.type = 0 AND post.deleted=0');
         $query->andWhere(['<>', 'cover', '']);
         if (!empty($tag)) {
-            $query->joinWith(['postTags', 'postTags.tags']);
             $query->andFilterWhere(['like', 'tags.name', $tag]);
         }
         $currentPage = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
