11829e970b6f9db0d6f333a012391deb7d444210
kaldmax <kaldmax@hotmail.com>
2016-02-26 06:10:30 -0800

    SIMPLE CODE

--- views/explore/view.php
+++ views/explore/view.php
@@ -121,7 +121,7 @@ $this->ogImage = Yii::$app->imageresize->thump($model->cover, 500, 500, 'resize'
                                                 ?>
                                                 <li>
                                                     <?php $ttaUrl = Yii::$app->helper->urlTitle($tags->tags->name); ?>
-                                                    <a href="<?= Yii::$app->urlManager->createUrl(["//مقالات/{$category}/{$ttaUrl}"]) ?>"><label
+                                                    <a href="<?= Yii::$app->urlManager->createUrl(["مقالات/{$category}/{$ttaUrl}"]) ?>"><label
                                                             class="label label-default"><?php echo $tags->tags->name; ?></label></a>
                                                 </li>
                                                 <?php
