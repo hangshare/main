84fb7fcbd35cbf7dd74a435f1bd86c5656dacf26
kaldmax <kaldmax@hotmail.com>
2016-02-26 02:26:09 -0800

    SIMPLE CODE

--- config/console.php
+++ config/console.php
@@ -1,22 +1,41 @@
 <?php
-
 Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
-
 $params = require(__DIR__ . '/params.php');
-$db = require(__DIR__ . '/db.php');
-
 return [
-    'id' => 'basic-console',
+    'id' => 'hangshare-live-console',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log', 'gii'],
+    'bootstrap' => ['log'],
     'controllerNamespace' => 'app\commands',
     'modules' => [
         'gii' => 'yii\gii\Module',
     ],
     'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
+        'imageresize' => [
+            'class' => 'app\components\Imageresize',
         ],
+        'hitcounter' => [
+            'class' => 'app\components\Hitcounter',
+        ],
+        'helper' => [
+            'class' => 'app\components\Helper',
+        ],
+        'AwsEmail' => [
+            'class' => 'app\components\AwsEmail',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=main.cdb3bm2h7j5j.us-east-1.rds.amazonaws.com;port=3306;dbname=hangshare',
+            'username' => 'hangshare',
+            'password' => 'Khaled!23',
+//            'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=hangshare',
+//            'username' => 'root',
+//            'password' => '',
+            'enableSchemaCache' => true,
+            'schemaCache' => 'cache',
+            'schemaCacheDuration' => 3600,
+            'charset' => 'utf8',
+        ],
+
         'log' => [
             'targets' => [
                 [
@@ -25,7 +44,6 @@ return [
                 ],
             ],
         ],
-        'db' => $db,
     ],
     'params' => $params,
 ];
