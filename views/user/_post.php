1a9540d8575875b232dd8ea28873170fcc726b44
kaldmax <kaldmax@hotmail.com>
2016-02-26 06:31:58 -0800

    SIMPLE CODE

--- views/user/_post.php
+++ views/user/_post.php
@@ -5,23 +5,28 @@ use yii\helpers\Html;
 if (isset($model->post)) {
     $model = $model->post;
 }
-$url = Yii::$app->urlManager->createAbsoluteUrl(['//explore/view', 'id' => $model->id, 'title' => $model->title]);
+
 $thump = Yii::$app->imageresize->thump($model->cover, 400, 290, 'crop');
 ?>
 <li>
     <div class="ex-all">
-        <a href="<?= $url ?>">
+        <a href="<?= $model->url ?>">
             <?php echo Html::img($thump, ['class' => 'img-responsive zoom-tilt']); ?>
         </a>
         <div class="shareblogsimi">
             <ul class="list-inline">
-                <li><a class="btn btn-primary js-share js-share-fasebook" post-url="<?= $url ?>"><i style="margin: 3px;" class="fa fa-fw fa-facebook"></i>شارك</a></li>
-                <li><a class="btn js-share js-share-twitter"  post-url="<?= $url ?>" style="color: #fff; background-color: #3E4347;"><i style="margin: 3px;" class="fa fa-twitter"></i>غرد</a></li>
-                <li><a class="btn js-share js-share-gpuls"  post-url="<?= $url ?>" style="color: #fff; background-color: #e51717;"><i style="margin: 3px;" class="fa fa-google-plus"></i> شارك</a></li>
+                <li><a class="btn btn-primary js-share js-share-fasebook" post-url="<?= $model->url ?>"><i
+                            style="margin: 3px;" class="fa fa-fw fa-facebook"></i>شارك</a></li>
+                <li><a class="btn js-share js-share-twitter" post-url="<?= $model->url ?>"
+                       style="color: #fff; background-color: #3E4347;"><i style="margin: 3px;"
+                                                                          class="fa fa-twitter"></i>غرد</a></li>
+                <li><a class="btn js-share js-share-gpuls" post-url="<?= $model->url ?>"
+                       style="color: #fff; background-color: #e51717;"><i style="margin: 3px;"
+                                                                          class="fa fa-google-plus"></i> شارك</a></li>
             </ul>
         </div>
     </div>
-    <a href="<?= $url ?>">
+    <a href="<?= $model->url ?>">
         <div class="col-md-12 titlein">
             <h4 style="overflow: hidden;text-overflow: ellipsis;white-space: nowrap;"><?php echo $model->title; ?></h4>
         </div>
