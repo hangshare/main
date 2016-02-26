a470325ab0878731b9037d4b26fe10c393a66144
kaldmax <kaldmax@hotmail.com>
2016-02-26 14:13:22 -0800

    SIMPLE CODE

--- components/Init.php
+++ components/Init.php
@@ -19,6 +19,11 @@ class Init extends Component {
             header("Location: {$post->url}");
             exit(0);
         }
+        if(strpos(Yii::$app->request->url,'explore/view')){
+            header("HTTP/1.1 301 Moved Permanently");
+            header("Location: http://www.hangshare.com/%D9%85%D9%82%D8%A7%D9%84%D8%A7%D8%AA/");
+            exit(0);
+        }
         parent::init();
     }
 
