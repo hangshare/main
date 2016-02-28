33db642a7ff419cc4d3c73de24c5a16669486241
kaldmax <kaldmax@hotmail.com>
2016-02-27 23:16:01 -0800

    SIMPLE CODE

--- commands/CronController.php
+++ commands/CronController.php
@@ -10,6 +10,8 @@ namespace app\commands;
 
 use Yii;
 use yii\console\Controller;
+use app\commands\User;
+use app\commands\Post;
 
 class CronController extends Controller
 {
@@ -32,6 +34,63 @@ class CronController extends Controller
         }
     }
 
+    public function actionSitemap(){
+        $posts= Yii::$app->db->createCommand("SELECT t.id, t.title, t.urlTitle, t.created_at FROM  post t WHERE t.deleted = 0 ORDER BY t.id DESC;")->queryAll();
+        $sitemap = new \DomDocument('1.0', 'UTF-8');
+        $sitemap->preserveWhiteSpace = false;
+        $sitemap->formatOutput = true;
+        $root = $sitemap->createElement("urlset");
+        $sitemap->appendChild($root);
+        $root_attr = $sitemap->createAttribute('xmlns');
+        $root->appendChild($root_attr);
+        $root_attr_text = $sitemap->createTextNode('http://www.sitemaps.org/schemas/sitemap/0.9');
+        $root_attr->appendChild($root_attr_text);
+        foreach ($posts as $post) {
+            $link = "www.hangshare.com/{$post['urlTitle']}/";
+            $url = $sitemap->createElement('url');
+            $root->appendChild($url);
+            $loc = $sitemap->createElement("loc");
+            $lastmod = $sitemap->createElement("lastmod");
+            $changefreq = $sitemap->createElement("changefreq");
+            $url->appendChild($loc);
+            $url_text = $sitemap->createTextNode($link);
+            $loc->appendChild($url_text);
+            $url->appendChild($lastmod);
+            $lastmod_text = $sitemap->createTextNode(date("Y-m-d", strtotime($post['created_at'])));
+            $lastmod->appendChild($lastmod_text);
+            $url->appendChild($changefreq);
+            $changefreq_text = $sitemap->createTextNode("weekly");
+            $changefreq->appendChild($changefreq_text);
+        }
+        $users= Yii::$app->db->createCommand("SELECT t.id, t.created_at, t.name FROM user t WHERE 1 ORDER BY t.id DESC;")->queryAll();
+        foreach ($users as $user) {
+            $link = "www.hangshare.com/user/{$user['id']}/";
+            $url = $sitemap->createElement('url');
+            $root->appendChild($url);
+
+            $loc = $sitemap->createElement("loc");
+            $lastmod = $sitemap->createElement("lastmod");
+            $changefreq = $sitemap->createElement("changefreq");
+
+            $url->appendChild($loc);
+            $url_text = $sitemap->createTextNode($link);
+            $loc->appendChild($url_text);
+
+            $url->appendChild($lastmod);
+            $lastmod_text = $sitemap->createTextNode(date("Y-m-d", strtotime($user['created_at'])));
+            $lastmod->appendChild($lastmod_text);
+
+            $url->appendChild($changefreq);
+            $changefreq_text = $sitemap->createTextNode("weekly");
+            $changefreq->appendChild($changefreq_text);
+        }
+
+        $file = Yii::$app->basePath . "/web/sitemap.xml";
+        echo $file;
+        $fh = fopen($file, 'w') or die("Can't open the sitemap file.");
+        fwrite($fh, $sitemap->saveXML());
+        fclose($fh);
+    }
 }
 
 
