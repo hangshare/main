a157baa77ef446302d4e2b010dc48571d4ebea85
kaldmax <kaldmax@hotmail.com>
2016-03-18 16:52:14 +0200

    SIMPLE CODE

--- components/Init.php
+++ components/Init.php
@@ -6,24 +6,26 @@ use Yii;
 use yii\base\Component;
 use app\models\Post;
 
-class Init extends Component {
+class Init extends Component
+{
 
-    public function init() {
+    public function init()
+    {
 
         if (strpos(Yii::$app->request->url, 'explore/') !== false && strpos(Yii::$app->request->url, 'title') !== false) {
             $id = $this->get_string_between(Yii::$app->request->url, 'id=', '&');
-            if(!is_numeric($id)){
+            if (!is_numeric($id)) {
                 $id = $this->get_string_between(Yii::$app->request->url, 'id=', '?title=');
             }
-            if(!is_numeric($id)){
+            if (!is_numeric($id)) {
                 $id = $this->get_string_between(Yii::$app->request->url, 'explore/', '/?');
             }
-            if(!is_numeric($id)){
+            if (!is_numeric($id)) {
                 $id = $this->get_string_between(Yii::$app->request->url, 'explore/', '?title');
             }
             $post = Post::findOne(['id' => $id]);
 
-            if(!isset($post)){
+            if (!isset($post)) {
                 header("Location: http://www.hangshare.com/%D9%85%D9%82%D8%A7%D9%84%D8%A7%D8%AA/");
                 exit(0);
             }
@@ -32,12 +34,18 @@ class Init extends Component {
             exit(0);
         }
 
-        if(strpos(Yii::$app->request->url,'explore/view') !== false){
+        if (strpos(Yii::$app->request->url, '//') !== false) {
+            header("HTTP/1.1 301 Moved Permanently");
+            header("Location: http://www.hangshare.com/");
+            exit(0);
+        }
+
+        if (strpos(Yii::$app->request->url, 'explore/view') !== false) {
             header("HTTP/1.1 301 Moved Permanently");
             header("Location: http://www.hangshare.com/%D9%85%D9%82%D8%A7%D9%84%D8%A7%D8%AA/");
             exit(0);
         }
-        if(strpos(Yii::$app->request->url,'site/index') !== false){
+        if (strpos(Yii::$app->request->url, 'site/index') !== false) {
             header("HTTP/1.1 301 Moved Permanently");
             header("Location: http://www.hangshare.com/");
             exit(0);
