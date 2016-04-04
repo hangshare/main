2d72581569569d65eb12e9bfa704b4043f22bdf5
kaldmax <kaldmax@hotmail.com>
2016-04-04 16:05:52 +0300

    S3

--- components/Imageresize.php
+++ components/Imageresize.php
@@ -99,9 +99,9 @@ class Imageresize extends Component
         }
 
 //        if (filemtime($thumppath) < (time() - 98000)) {
-//            return 'https://dw4xox9sj1rhd.cloudfront.net/' . $fileExtract[0] . '/' . $filethump . '/' . $fileExtract[1];
+            return 'https://dw4xox9sj1rhd.cloudfront.net/' . $fileExtract[0] . '/' . $filethump . '/' . $fileExtract[1];
 //        } else {
-            return Url::home(true) . 'media/' . $fileExtract[0] . '/' . $filethump . '/' . $fileExtract[1];
+//            return Url::home(true) . 'media/' . $fileExtract[0] . '/' . $filethump . '/' . $fileExtract[1];
 //        }
 
     }
