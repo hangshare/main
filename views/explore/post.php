d81a079985c787418b9b5ea9e6203c98939c01d9
kaldmax <kaldmax@hotmail.com>
2016-04-05 05:08:03 +0300

    A

--- views/explore/post.php
+++ views/explore/post.php
@@ -9,7 +9,7 @@ use yii\widgets\ActiveForm;
 /* @var $model app\models\Post */
 /* @var $form yii\widgets\ActiveForm */
 $this->title = 'موضوع جديد';
-$thump =  Yii::$app->imageresize->thump($model->cover, 150, 100, 'crop');
+$thump = Yii::$app->imageresize->thump($model->cover, 150, 100, 'crop');
 foreach ($model->postBodies as $data) {
     $model->body .= $data->body;
 }
@@ -18,30 +18,30 @@ foreach ($model->postBodies as $data) {
     <div class="post-form col-md-9">
         <div class="white pad">
             <h3>معلومات المقالة</h3>
-            <hr />
+            <hr/>
             <?php
             $form = ActiveForm::begin(['options' => [
-                            'id' => 'add-post'
-                            , 'enctype' => 'multipart/form-data']])
+                'id' => 'add-post'
+                , 'enctype' => 'multipart/form-data']])
             ?>
+
             <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
             <label>صورة الغلاف</label>
+
             <div class="row">
-                <div class="col-xs-3">
+                <div class="col-md-2">
                     <?php echo Html::img($thump, ['class' => 'img-responsive', 'id' => 'coveri']); ?>
-                </div>
-                <div class="col-xs-9 centimpost">
-                    <input disabled="disabled" placeholder="select file" id="uploadFile">
-                    <div class="fileUpload btn btn-primary rela">
+                    <button class="btn btn-primary btn-block" id="uploadtos3">
                         <span>اختر صورة</span>
-                        <?= $form->field($model, 'cover_file')->fileInput()->label(false) ?>
-                    </div>
+                    </button>
                 </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-md-12">
-                    <?= $form->field($model, 'ylink')->textInput(['maxlength' => true]) ?>
+                <div class="col-md-1">
+                    <span style="font-size: 30px;top: 22px; position: relative;">
+                        أو
+                    </span>
+                </div>
+                <div class="col-md-9">
+                    <?= $form->field($model, 'ylink')->textInput(['maxlength' => true, 'placeholder'=> 'مثال: https://www.youtube.com/watch?v=K5sqorZ9x7o']) ?>
                 </div>
             </div>
             <br>
@@ -62,12 +62,12 @@ foreach ($model->postBodies as $data) {
             <?= $form->field($model, 'body')->textarea(['class' => 'froala-edit']) ?>
             <?=
             $form->field($model, 'tags')->checkboxList(ArrayHelper::map(
-                            Tags::find()
-                                    ->where('type=1')
-                                ->select('id, name')
-                                ->orderBy('id desc')
-                                ->limit(50)
-                                    ->all(), 'id', 'name'))
+                Tags::find()
+                    ->where('type=1')
+                    ->select('id, name')
+                    ->orderBy('id desc')
+                    ->limit(50)
+                    ->all(), 'id', 'name'))
             ?>
 
             <label>التصنيفات الاضافية</label>
@@ -115,17 +115,30 @@ foreach ($model->postBodies as $data) {
         <div class="row">
             <div class="white-box" style="border: 1px solid #51B415;">
                 <h3 style="margin-top: 10px;">ملاحظة : </h3>
+
                 <p>سوف يتم مراجعة وتدقيق المقالة قبل نشرها بشكل رسمي على الموقع</p>
             </div>
         </div>
         <div class="row m-t-25">
             <div class="white-box">
                 <h3 style="margin-top: 10px;">نصائح :</h3>
-                <p><b>العنوان : </b> إختيار العنوان المناسب يساعد على وصول المقالة لأكبر عدد من الأشخاص المهتمين ، يرجى اختيار جملة قصيرة تتكون من 4 كلمات على الأقل و 10 كلمات كحد أقصى.</p>                
-                <p><b>صورة الغلاف : </b> اختيار صورة اغلاف جميلة تجذب الأشخاص الى قراءة مقالتك ، يرجى اختيار صورة يقل حجمها عن 3 MB . </p>
-                <p><b>الموضوع: </b> يفضل ان يكون الموضوع متوسط الحجم يزيد عن عشر اسطر ، يمكنك تحميل صور او ارفاق فيديو وتنسيق النص بشكل جميل.</p>
-                <p><b>الكلمات المفتاحية : </b> اختيار الكلمات المفتاحية المناسبة تسهل عملية البحث وبالتالي زيادة ظهور المقالة .</p>
+
+                <p><b>العنوان : </b> إختيار العنوان المناسب يساعد على وصول المقالة لأكبر عدد من الأشخاص المهتمين ، يرجى
+                    اختيار جملة قصيرة تتكون من 4 كلمات على الأقل و 10 كلمات كحد أقصى.</p>
+
+                <p><b>صورة الغلاف : </b> اختيار صورة اغلاف جميلة تجذب الأشخاص الى قراءة مقالتك ، يرجى اختيار صورة يقل
+                    حجمها عن 3 MB . </p>
+
+                <p><b>الموضوع: </b> يفضل ان يكون الموضوع متوسط الحجم يزيد عن عشر اسطر ، يمكنك تحميل صور او ارفاق فيديو
+                    وتنسيق النص بشكل جميل.</p>
+
+                <p><b>الكلمات المفتاحية : </b> اختيار الكلمات المفتاحية المناسبة تسهل عملية البحث وبالتالي زيادة ظهور
+                    المقالة .</p>
             </div>
         </div>
     </div>
 </div>
+
+<from id="uploadform" method="POST" enctype="multipart/form-data">
+    <input id="files3" type="file" />
+</from>
