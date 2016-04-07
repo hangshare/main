122d9b14e809b577f42ad5e49f43c89728a6a8e1
kaldmax <kaldmax@hotmail.com>
2016-04-07 14:50:11 +0300

    SIMPLE CODE

--- views/site/_sitemappost.php
+++ views/site/_sitemappost.php
@@ -2,5 +2,5 @@
 use yii\helpers\Html;
 ?>
 <li>
-    <a href="<?php echo Yii::$app->urlManager->createUrl(['//explore/view', 'id' => $model->id, 'title' => $model->title]); ?>"><?php echo $model->title; ?></a>
+    <a href="<?= $model->url; ?>"><?php echo $model->title; ?></a>
 </li>
