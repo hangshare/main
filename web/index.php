25b239e8ed5801f21b53a96fc89855bad03370bd
kaldmax <kaldmax@hotmail.com>
2016-04-13 12:03:24 +0300

    SIMPLE CODE

--- web/index.php
+++ web/index.php
@@ -5,6 +5,10 @@
 //defined('YII_DEBUG') or define('YII_DEBUG', true);
 //defined('YII_ENV') or define('YII_ENV', 'dev');
 
+
+error_reporting(0);
+ini_set('display_errors', 0);
+
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
