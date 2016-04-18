9eb936467bf173fbc1b8351f7398f56c1def4e4e
kaldmax <kaldmax@hotmail.com>
2016-04-18 04:04:31 +0300

    SIMPLE CODE

--- config/web.php
+++ config/web.php
@@ -34,28 +34,43 @@ $config = [
         'assetManager' => [
             'class' => 'yii\web\AssetManager',
             'forceCopy' => true,
-            'bundles' => [
-                'yii\web\YiiAsset' => [
-                    'js' => [
-                        'yii.min.js'
-                    ]
-                ],
-                'yii\web\JqueryAsset' => [
-                    'js' => [
-                        'jquery.min.js'
-                    ]
-                ],
-                'yii\bootstrap\BootstrapPluginAsset' => [
-                    'js' => [
-                        'js/bootstrap.min.js',
-                    ]
-                ],
-                'yii\bootstrap\BootstrapAsset' => [
-                    'css' => [
-                        'css/bootstrap.min.css',
-                    ]
-                ],
-            ]
+            'assetMap' => [
+                'jquery.js' => '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
+                'yii.js'=>'https://s3.amazonaws.com/hangshare.static/assets/yii.min.js',
+                'bootstrap.js'=>'https://s3.amazonaws.com/hangshare.static/assets/bootstrap.min.js',
+                'yii.activeForm.min.js'=>'https://s3.amazonaws.com/hangshare.static/assets/yii.activeForm.min.js',
+                'yii.validation.js'=>'https://s3.amazonaws.com/hangshare.static/assets/yii.validation.min.js',
+                'select2.full.min.js'=>'https://s3.amazonaws.com/hangshare.static/assets/select2.full.min.js',
+                'select2-krajee.min.js'=>'https://s3.amazonaws.com/hangshare.static/assets/select2-krajee.min.js',
+                'ar.js'=>'https://s3.amazonaws.com/hangshare.static/assets/ar.min.js',
+                'bootstrap.css'=>'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.2.0-rc2/css/bootstrap-rtl.css',
+                'kv-widgets.min.css'=>'https://s3.amazonaws.com/hangshare.static/assets/kv-widgets.min.css',
+                'select2.min.css'=>'https://s3.amazonaws.com/hangshare.static/assets/select2.min.css',
+                'select2-addl.min.css'=>'https://s3.amazonaws.com/hangshare.static/assets/select2-addl.min.css',
+                'select2-krajee.min.css'=>'https://s3.amazonaws.com/hangshare.static/assets/select2-krajee.min.css',
+            ],
+//            'bundles' => [
+//                'yii\web\YiiAsset' => [
+//                    'js' => [
+//                        'yii.min.js'
+//                    ]
+//                ],
+//                'yii\web\JqueryAsset' => [
+//                    'js' => [
+//                        'jquery.min.js'
+//                    ]
+//                ],
+//                'yii\bootstrap\BootstrapPluginAsset' => [
+//                    'js' => [
+//                        'js/bootstrap.min.js',
+//                    ]
+//                ],
+//                'yii\bootstrap\BootstrapAsset' => [
+//                    'css' => [
+//                        'css/bootstrap.min.css',
+//                    ]
+//                ],
+//            ]
         ],
         'urlManager' => [
             'enablePrettyUrl' => true,
