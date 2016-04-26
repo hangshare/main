efd9b9636e14a0eea639ed93782b10a9799227e1
kaldmax <kaldmax@hotmail.com>
2016-04-26 17:15:05 +0300

    SIMPLE CODE

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,5 +1,6 @@
 <?php
 use kartik\alert\Alert;
+
 ?>
 <?php
 $this->beginContent('@app/views/layouts/htmlhead.php');
@@ -10,9 +11,16 @@ $this->beginContent('@app/views/layouts/header.php');
 $this->endContent();
 ?>
 <?php
-if(count(Yii::$app->getSession()->getAllFlashes())> 0) {
-//    echo Alert::widget();
-}
+if (count(Yii::$app->getSession()->getAllFlashes()) > 0): ?>
+    <?php foreach (Yii::$app->getSession()->getAllFlashes() as $key => $value) : ?>
+        <div class="container">
+            <div class="m-t-25">
+                <div class="alert text-center alert-<?= $key ?>"> <?= Yii::$app->session->getFlash($key) ?></div>
+            </div>
+        </div>
+        <?php
+    endforeach;
+endif;
 ?>
 <div class="main">
     <?= $content ?>
