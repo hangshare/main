8f66cd9211830f487a54592a1b844425e977cb20
kaldmax <kaldmax@hotmail.com>
2016-04-26 17:55:15 +0300

    SIMPLE CODE

--- views/site/resetPassword.php
+++ views/site/resetPassword.php
@@ -12,11 +12,11 @@ $this->title = 'تعيين كلمة مرور جديدة';
 <div class="container">
     <div class="white-box">
         <h1><?= Html::encode($this->title) ?></h1>
-        <p>يرجى اختيار كلمة مرور مناسبة :</p>
         <div class="row">
             <div class="col-lg-5">
                 <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
                 <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= $form->field($model, 'repeat_password')->passwordInput() ?>
                 <div class="form-group">
                     <?= Html::submitButton('حفظ', ['class' => 'btn btn-primary res-full']) ?>
                 </div>
