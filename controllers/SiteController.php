666f58458e88bff2bca71c021276fb00db599019
kaldmax <kaldmax@hotmail.com>
2016-04-26 17:55:03 +0300

    SIMPLE CODE

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -655,6 +655,7 @@ class SiteController extends Controller
 
     public function actionResetpassword($token)
     {
+        $token = substr($token, 0, -1);
         try {
             $model = new ResetPasswordForm($token);
         } catch (InvalidParamException $e) {
@@ -662,7 +663,7 @@ class SiteController extends Controller
         }
         if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->resetPassword()) {
             Yii::$app->getSession()->setFlash('success', 'تم تغير كلمة السر بنجاح.');
-            return $this->goHome();
+            return $this->redirect(['//login']);
         }
         return $this->render('resetPassword', [
             'model' => $model,
