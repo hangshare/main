c0980e1b0bda8dc28e4b9bed093e216af9422af8
kaldmax <kaldmax@hotmail.com>
2016-05-11 13:58:16 +0300

    SIMPLE CODE

--- views/user/success.php
+++ views/user/success.php
@@ -4,7 +4,7 @@ $this->title = 'تمت عملية طلب النقود بنجاح';
 ?>
 <div class="container">
     <div class="white-box">
-        <h1 class="text-center">    تم ارسال طلب التحويل بنحاح سوف يتم تحويل المبلغ خلال 48 ساعة من الآن.</h1>
+        <h1 class="text-center">تم ارسال طلب التحويل بنحاح ، سوف نقوم بتحويل المبلغ اليكم خلال خمس أيام عمل.</h1>
         <br>
         <br>
         <div class="text-center">
