b829e1645a59dd4f9b3017512bf7a6a2a61a527d
kaldmax <kaldmax@hotmail.com>
2016-05-18 03:21:41 +0300

    SIMPLE CODE

--- views/explore/view.php
+++ views/explore/view.php
@@ -165,17 +165,6 @@ $fUrl = $helper->getLoginUrl('http://www.hangshare.com/site/facebook/', $params)
                                         <script>
                                             (adsbygoogle = window.adsbygoogle || []).push({});
                                         </script>
-                                    <?php else : ?>
-                                        <script async
-                                                src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                                        <!-- Leaderboard - Post Bottom -->
-                                        <ins class="adsbygoogle"
-                                             style="display:inline-block;width:728px;height:90px"
-                                             data-ad-client="ca-pub-6288640194310142"
-                                             data-ad-slot="1341443712"></ins>
-                                        <script>
-                                            (adsbygoogle = window.adsbygoogle || []).push({});
-                                        </script>
                                     <?php endif; ?>
                                 </div>
                             </div>
