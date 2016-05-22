5576189b8a58385532ded2260330e8cf2ffe35de
kaldmax <kaldmax@hotmail.com>
2016-05-22 21:34:29 +0300

    SIMPLE CODE

--- views/explore/view.php
+++ views/explore/view.php
@@ -221,26 +221,6 @@ $fUrl = $helper->getLoginUrl('http://www.hangshare.com/site/facebook/', $params)
                             </div>
                         </div>
                 </article>
-                <!--                <hr/>-->
-                <!--                <div class="row">-->
-                <!--                    <div class="col-md-12">-->
-                <!--                        <h3>التعليقات على موقع الفيسبوك</h3>-->
-
-                <!--                        <div id="fb-root"></div>-->
-                <!--                        <script>(function (d, s, id) {-->
-                <!--                                var js, fjs = d.getElementsByTagName(s)[0];-->
-                <!--                                if (d.getElementById(id))-->
-                <!--                                    return;-->
-                <!--                                js = d.createElement(s);-->
-                <!--                                js.id = id;-->
-                <!--                                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=1024611190883720";-->
-                <!--                                fjs.parentNode.insertBefore(js, fjs);-->
-                <!--                            }(document, 'script', 'facebook-jssdk'));</script>-->
-                <!--                        <div class="fb-comments" data-href="-->
-                <?php //echo $model->url ?><!--" data-numposts="5"-->
-                <!--                             data-width="100%"></div>-->
-                <!--                    </div>-->
-                <!--                </div>-->
             </div>
             <?php if (!Yii::$app->helper->isMobile()): ?>
                 <div class="col-md-3" style="left: 0;position: fixed;">
