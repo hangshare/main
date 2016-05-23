18159986ae2e5f516dd597ec3e29c2f127eb9ef8
kaldmax <kaldmax@hotmail.com>
2016-05-23 23:49:14 +0300

    SIMPLE CODE

--- views/explore/view.php
+++ views/explore/view.php
@@ -16,6 +16,20 @@ $helper = $fb->getRedirectLoginHelper();
 $params = ['scope' => 'email,user_about_me'];
 $fUrl = $helper->getLoginUrl('http://www.hangshare.com/site/facebook/', $params);
 ?>
+
+
+<?php if (Yii::$app->helper->isMobile()) : ?>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- responsive - new mobile upper post -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-6288640194310142"
+         data-ad-slot="9020008518"
+         data-ad-format="auto"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+<?php endif; ?>
     <div class="center m-t-8">
         <?php if (Yii::$app->helper->isMobile()) : ?>
             <script async
