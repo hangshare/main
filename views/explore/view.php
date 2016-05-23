1054f9ae94adb66d6f012d78325f9b5d73844743
kaldmax <kaldmax@hotmail.com>
2016-05-23 19:32:47 +0300

    SIMPLE CODE

--- views/explore/view.php
+++ views/explore/view.php
@@ -98,13 +98,12 @@ $fUrl = $helper->getLoginUrl('http://www.hangshare.com/site/facebook/', $params)
                     <div class="post-body">
                         <div class="center m-t-8">
                             <?php if (Yii::$app->helper->isMobile()) : ?>
-                                <script async
-                                        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                                <!-- Responsive - Mobile Post Upper -->
+                                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                <!-- responsive - new mobile upper post -->
                                 <ins class="adsbygoogle"
-                                     style="display:block"
+                                     style="display:block;height: 250px"
                                      data-ad-client="ca-pub-6288640194310142"
-                                     data-ad-slot="6756705310"
+                                     data-ad-slot="9020008518"
                                      data-ad-format="auto"></ins>
                                 <script>
                                     (adsbygoogle = window.adsbygoogle || []).push({});
@@ -148,7 +147,7 @@ $fUrl = $helper->getLoginUrl('http://www.hangshare.com/site/facebook/', $params)
                                                 src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                                         <!-- Responsive - Mobile Post Bottom -->
                                         <ins class="adsbygoogle"
-                                             style="display:block"
+                                             style="display:block;height: 250px;"
                                              data-ad-client="ca-pub-6288640194310142"
                                              data-ad-slot="9710171716"
                                              data-ad-format="auto"></ins>
