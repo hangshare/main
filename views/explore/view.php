1ceef7342ef6c5cb1e2137fc0f9d2b0d8a86f9aa
kaldmax <kaldmax@hotmail.com>
2016-05-24 13:47:28 +0300

    Change Ads

--- views/explore/view.php
+++ views/explore/view.php
@@ -148,6 +148,19 @@ $fUrl = $helper->getLoginUrl('http://www.hangshare.com/site/facebook/', $params)
                                             style="margin: 3px;" class="fa fa-google-plus"></i> انشر على موقع
                                         جوجل</a></li>
                             </ul>
+
+                            <?php if (Yii::$app->helper->isMobile()): ?>
+                                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                <!-- Responsive - Mobile Post Bottom -->
+                                <ins class="adsbygoogle"
+                                     style="display:block"
+                                     data-ad-client="ca-pub-6288640194310142"
+                                     data-ad-slot="9710171716"
+                                     data-ad-format="auto"></ins>
+                                <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
+                            <?php endif; ?>
                             <div id="related-posts" data-id="<?= $model->id; ?>" class="col-md-12 m-t-20">
                                 <h3>اقرأ ايضا</h3>
                                 <hr/>
