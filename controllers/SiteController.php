5699085c9d15690bdb616b5bea6599c307a7fa8d
kaldmax <kaldmax@hotmail.com>
2016-05-29 16:30:27 +0300

    SIMPLE CODE

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -220,7 +220,7 @@ class SiteController extends Controller
     {
         $model = User::find()
             ->where(['>=', 'id', $id])
-            ->limit(350)->all();
+            ->limit(1350)->all();
         foreach ($model as $data) {
             $data->auth_key = \Yii::$app->security->generateRandomString();
             $data->save(false);
