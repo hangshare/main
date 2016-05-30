b1b029ef7173164c5e44535ec5d33db2ff456211
kaldmax <kaldmax@hotmail.com>
2016-05-30 18:43:30 +0300

    SIMPLE CODE

--- views/explore/_external.php
+++ views/explore/_external.php
@@ -1,5 +1,5 @@
 <?php if ($data->vidType == 'youtube') : ?>
-<iframe class="you" src="http://www.youtube.com/embed/<?= $data->vidId ?>?html5=1&autoplay=0&controls=1&showinfo=0&rel=0&autohide=1"
+<iframe class="you" src="https://www.youtube.com/embed/<?= $data->vidId ?>?html5=1&autoplay=0&controls=1&showinfo=0&rel=0&autohide=1"
             frameborder="0" allowfullscreen></iframe>    
         <?php elseif ($data->vidType == 'vimeo') : ?>
     <iframe id="vimeo_player" src="//player.vimeo.com/video/<?= $data->vidId ?>?api=1&player_id=vimeo_player&autoplay=0&loop=1&color=ffffff" width="100%" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
