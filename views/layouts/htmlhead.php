5e816d6257ac96c505d6547dc4eea6d9f3ab9c9b
kaldmax <kaldmax@hotmail.com>
2016-06-09 01:21:07 +0300

    SIMPLE CODE

--- views/layouts/htmlhead.php
+++ views/layouts/htmlhead.php
@@ -17,72 +17,113 @@ $canonical = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-    <head>
-        <meta charset="<?= Yii::$app->charset ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="<?= $this->description ?>" />
-        <?= Html::csrfMetaTags() ?>
-        <!-- Facebook meta tags -->
-        <meta property="og:title" content="<?= Html::encode($this->title) ?>" />
-        <meta property="og:site_name" content="hangshare"/>
-        <meta property="og:url" content="<?= $canonical ?>" />
-        <meta property="og:description" content="<?= $this->description; ?>" />
-        <meta property="fb:app_id" content="1024611190883720" />
-        <meta property="og:image" content="<?= $this->ogImage; ?>" />
-        <meta property="og:type" content="article" />
-        <meta property="article:author" content="hangshare" />
-        <meta property="article:publisher" content="http://www.hangshare.com" />
-        <title><?= Html::encode($this->title) ?></title>
-        <?php $this->head() ?>
-        <script>
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?= $this->description ?>"/>
+    <?= Html::csrfMetaTags() ?>
+    <!-- Facebook meta tags -->
+    <meta property="og:title" content="<?= Html::encode($this->title) ?>"/>
+    <meta property="og:site_name" content="hangshare"/>
+    <meta property="og:url" content="<?= $canonical ?>"/>
+    <meta property="og:description" content="<?= $this->description; ?>"/>
+    <meta property="fb:app_id" content="1024611190883720"/>
+    <meta property="og:image" content="<?= $this->ogImage; ?>"/>
+    <meta property="og:type" content="article"/>
+    <meta property="article:author" content="hangshare"/>
+    <meta property="article:publisher" content="http://www.hangshare.com"/>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
                     (i[r].q = i[r].q || []).push(arguments)
                 }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                        m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-            ga('create', 'UA-68983967-1', 'auto');
-            ga('send', 'pageview');
-        </script>
-        <!-- Facebook Pixel Code -->
-        <script>
-            !function (f, b, e, v, n, t, s) {
-                if (f.fbq)
-                    return;
-                n = f.fbq = function () {
-                    n.callMethod ?
-                            n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                };
-                if (!f._fbq)
-                    f._fbq = n;
-                n.push = n;
-                n.loaded = !0;
-                n.version = '2.0';
-                n.queue = [];
-                t = b.createElement(e);
-                t.async = !0;
-                t.src = v;
-                s = b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t, s)
-            }(window,
-                    document, 'script', '//connect.facebook.net/en_US/fbevents.js');
+        ga('create', 'UA-68983967-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq)
+                return;
+            n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq)
+                f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window,
+            document, 'script', '//connect.facebook.net/en_US/fbevents.js');
 
-            fbq('init', '1713055775580604');
-            fbq('track', "PageView");</script>
+        fbq('init', '1713055775580604');
+        fbq('track', "PageView");</script>
     <noscript><img height="1" width="1" style="display:none"
                    src="https://www.facebook.com/tr?id=1713055775580604&ev=PageView&noscript=1"
-                   /></noscript>
+        /></noscript>
     <!-- End Facebook Pixel Code -->
+    <!-- start Mixpanel -->
+    <script type="text/javascript">(function (e, b) {
+            if (!b.__SV) {
+                var a, f, i, g;
+                window.mixpanel = b;
+                b._i = [];
+                b.init = function (a, e, d) {
+                    function f(b, h) {
+                        var a = h.split(".");
+                        2 == a.length && (b = b[a[0]], h = a[1]);
+                        b[h] = function () {
+                            b.push([h].concat(Array.prototype.slice.call(arguments, 0)))
+                        }
+                    }
+
+                    var c = b;
+                    "undefined" !== typeof d ? c = b[d] = [] : d = "mixpanel";
+                    c.people = c.people || [];
+                    c.toString = function (b) {
+                        var a = "mixpanel";
+                        "mixpanel" !== d && (a += "." + d);
+                        b || (a += " (stub)");
+                        return a
+                    };
+                    c.people.toString = function () {
+                        return c.toString(1) + ".people (stub)"
+                    };
+                    i = "disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
+                    for (g = 0; g < i.length; g++)f(c, i[g]);
+                    b._i.push([a, e, d])
+                };
+                b.__SV = 1.2;
+                a = e.createElement("script");
+                a.type = "text/javascript";
+                a.async = !0;
+                a.src = "undefined" !== typeof MIXPANEL_CUSTOM_LIB_URL ? MIXPANEL_CUSTOM_LIB_URL : "file:" === e.location.protocol && "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//) ? "https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js" : "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";
+                f = e.getElementsByTagName("script")[0];
+                f.parentNode.insertBefore(a, f)
+            }
+        })(document, window.mixpanel || []);
+        mixpanel.init("b6fee6226edf2119b63f89bee40e1f3c");</script><!-- end Mixpanel -->
 </head>
 <body>
-    <?php foreach (Yii::$app->session->getAllFlashes() as $message):; ?>
-        <?php //$message['message']; ?>
-    <?php endforeach; ?>
-    <?php $this->beginBody() ?>
+<?php foreach (Yii::$app->session->getAllFlashes() as $message):; ?>
+    <?php //$message['message']; ?>
+<?php endforeach; ?>
+<?php $this->beginBody() ?>
     
