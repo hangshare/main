f8c05f3f3fcd8fde617b4c45905ddf87dd99822b
khaled <kaldmax@hotmail.com>
2016-08-28 05:54:23 -0600

    SIMPLE CODE

---
+++ views/explore/deleted.php
@@ -0,0 +1,90 @@
+<?php
+
+use app\models\Post;
+use app\models\Tags;
+use yii\helpers\Html;
+use yii\widgets\ListView;
+
+$this->title = Yii::t('app', 'title.deleted');
+$this->description = Yii::t('app', 'description.deleted');
+
+?>
+<div class="container m-t-25">
+    <div class="row">
+        <?php if (Yii::$app->helper->isMobile()) : ?>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- left right responsive categery -->
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-client="ca-pub-6288640194310142"
+                 data-ad-slot="8394999319"
+                 data-ad-format="auto"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        <?php else : ?>
+            <div style="margin: 0 auto; display: table;">
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- Leaderboard - Category Upper -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:728px;height:90px"
+                     data-ad-client="ca-pub-6288640194310142"
+                     data-ad-slot="6969174910"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
+        <?php endif ?>
+    </div>
+    <div class="col-md-9">
+        <div class="row">
+            <h1 class="normal"><?= $this->title ?></h1>
+        </div>
+        <hr class="nomargin m-b-20">
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                echo ListView::widget([
+                    'dataProvider' => $dataProvider,
+                    'itemView' => '_view',
+                    'layout' => "<ul class='list-unstyled inifi'>{items}\n</ul>",
+                    'itemOptions' => [
+                        'tag' => false
+                    ],
+                ]);
+                ?>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3  res-hidden">
+        <div class="row m-b-20">
+            <div class="col-md-12">
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- left right responsive categery -->
+                <ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-6288640194310142"
+                     data-ad-slot="8394999319"
+                     data-ad-format="auto"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
+        </div>
+        <div class="row m-t-20">
+            <div style="position: relative">
+                <div class="m-t-25">
+                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <!-- MPU - Category Right -->
+                    <ins class="adsbygoogle"
+                         style="display:inline-block;width:300px;height:250px"
+                         data-ad-client="ca-pub-6288640194310142"
+                         data-ad-slot="9922641319"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
