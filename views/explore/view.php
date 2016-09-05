d8fc4dd4edd080e34941dcbe9853d1c44296013d
khaled <kaldmax@hotmail.com>
2016-09-05 15:55:19 -0600

    SIMPLE CODE

--- views/explore/view.php
+++ views/explore/view.php
@@ -104,7 +104,7 @@ $fUrl = $helper->getLoginUrl('https://www.hangshare.com/site/facebook/', $params
                                 $totalViews = $model->postStats->views;
                                 echo number_format($totalViews + 1);
                                 ?></li>
-                            <?php if (!Yii::$app->user->isGuest && Yii::$app->user->identity->id == $model->userId) : ?>
+                            <?php if (!Yii::$app->user->isGuest && (Yii::$app->user->identity->id == $model->userId || Yii::$app->user->identity->type)) : ?>
                                 <li class="divider"></li>
                                 <li><?= Html::a(Yii::t('app', 'Edit Post'), ['//explore/post', 'id' => $model->id]); ?></li>
                                 <li class="divider"></li>
