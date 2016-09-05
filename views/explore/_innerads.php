8a2569bb442a3d6a6e684a5eb446d90a61e6eb91
khaled <kaldmax@hotmail.com>
2016-09-05 15:14:41 -0600

    SIMPLE CODE

---
+++ views/explore/_innerads.php
@@ -0,0 +1,12 @@
+<li>
+    <script async
+            src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- Leaderboard - Post Upper -->
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:728px;height:90px"
+         data-ad-client="ca-pub-6288640194310142"
+         data-ad-slot="1011333310"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</li>
