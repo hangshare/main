809d24fe58e647a524a749653d4cff618a0695fd
khaled <kaldmax@hotmail.com>
2016-09-05 03:32:59 -0600

    SIMPLE CODE

--- components/HangUrl.php
+++ components/HangUrl.php
@@ -15,16 +15,10 @@ class HangUrl extends \yii\web\UrlManager
 {
     public function createUrl($params)
     {
-
-        if (!isset($params['language'])) {
-            if (Yii::$app->session->has('language'))
-                Yii::$app->language = Yii::$app->session->get('language');
-            else if(isset(Yii::$app->request->cookies['language']))
-                Yii::$app->language = Yii::$app->request->cookies['language']->value;
+        if (Yii::$app->language == 'en') {
             $params['language']=Yii::$app->language;
         }
         return parent::createUrl($params);
-
 
 //        $url = parent::createUrl($params);
 //
