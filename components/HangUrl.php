0c44950873266d28d89e1df91743cd31f803bbdd
khaled <kaldmax@hotmail.com>
2016-09-05 04:45:03 -0600

    SIMPLE CODE

--- components/HangUrl.php
+++ components/HangUrl.php
@@ -27,54 +27,22 @@ class HangUrl extends \yii\web\UrlManager
         $url = str_replace('/e/', 'en', $url);
         $url = str_replace('en/en', 'en', $url);
         return $url;
-//        $url = parent::createUrl($params);
-//        if (Yii::$app->language == 'en') {
-//            if (strpos('en/', $url) === false) {
-//                $url = 'en' . $url;
-//            }
-//            $url = str_replace('/e/', '/en/', $url);
-//
-//            $url = str_replace('earticles', 'en/articles', $url);
-//            $url = str_replace('een', 'en', $url);
-//            $url = str_replace('en/en', 'en', $url);
-//            $url = str_replace('en/een', 'en', $url);
-//            $url = str_replace('en/e', 'en', $url);
-//            $url = str_replace('en/en', 'en', $url);
-//        }
-//        return $url;
-
-//        $url = parent::createUrl($params);
-//
-//        if (Yii::$app->language == 'en') {
-////            $params['language'] = Yii::$app->language;
-////            $url = str_replace('.com', '.com/en', $url);
-////            $url = str_replace('hangshare/web', 'hangshare/web/en', $url);
-////
-//
-//            $url = 'en'.$url;
-
-//
-//        }
-//        $url = str_replace('en/en/en', 'en', $url);
-//        $url = str_replace('en/en', 'en', $url);
-//        return $url;
     }
 
     public function createAbsoluteUrl($params)
     {
-        if (Yii::$app->language == 'en')
-            $params['language'] = Yii::$app->language;
 
-        $url = parent::createAbsoluteUrl($params);
-        if (strpos($url, 'language=en') !== false) {
-            $url = str_replace('.com', '.com/en', $url);
-            $url = str_replace('hangshare/web', 'hangshare/web/en', $url);
-            $url = str_replace('?language=en', '', $url);
+        if (!isset($params['language']) && Yii::$app->language == 'en') {
+            $params['language'] = Yii::$app->language;
         }
-        $url = str_replace('/e/', '/en/', $url);
+        $url =parent::createAbsoluteUrl($params);
+        if (strpos($url, 'language=en') !== false) {
+            $url = str_replace('?language=en', '', $url);
+            $url = '/en'.$url;
+        }
+
+        $url = str_replace('/e/', 'en', $url);
         $url = str_replace('en/en', 'en', $url);
-        $url = str_replace('earticles', 'en/articles', $url);
-        $url = str_replace('een', 'en', $url);
         return $url;
     }
 }
