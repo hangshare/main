0cc18a06464387d0615b68c9f1325bdef7a63f49
khaled <kaldmax@hotmail.com>
2016-09-05 03:29:35 -0600

    SIMPLE CODE

--- components/HangUrl.php
+++ components/HangUrl.php
@@ -15,24 +15,35 @@ class HangUrl extends \yii\web\UrlManager
 {
     public function createUrl($params)
     {
-        $url = parent::createUrl($params);
 
-        if (Yii::$app->language == 'en') {
-//            $params['language'] = Yii::$app->language;
-//            $url = str_replace('.com', '.com/en', $url);
-//            $url = str_replace('hangshare/web', 'hangshare/web/en', $url);
-//            $url = str_replace('?language=en', '', $url);
-
-            $url = 'en'.$url;
-//            $url = str_replace('/e/', '/en/', $url);
-//            $url = str_replace('en/en', 'en', $url);
-//            $url = str_replace('earticles', 'en/articles', $url);
-//            $url = str_replace('een', 'en', $url);
-
+        if (!isset($params['language'])) {
+            if (Yii::$app->session->has('language'))
+                Yii::$app->language = Yii::$app->session->get('language');
+            else if(isset(Yii::$app->request->cookies['language']))
+                Yii::$app->language = Yii::$app->request->cookies['language']->value;
+            $params['language']=Yii::$app->language;
         }
-        $url = str_replace('en/en/en', 'en', $url);
-        $url = str_replace('en/en', 'en', $url);
-        return $url;
+        return parent::createUrl($params);
+
+
+//        $url = parent::createUrl($params);
+//
+//        if (Yii::$app->language == 'en') {
+////            $params['language'] = Yii::$app->language;
+////            $url = str_replace('.com', '.com/en', $url);
+////            $url = str_replace('hangshare/web', 'hangshare/web/en', $url);
+////            $url = str_replace('?language=en', '', $url);
+//
+//            $url = 'en'.$url;
+////            $url = str_replace('/e/', '/en/', $url);
+////            $url = str_replace('en/en', 'en', $url);
+////            $url = str_replace('earticles', 'en/articles', $url);
+////            $url = str_replace('een', 'en', $url);
+//
+//        }
+//        $url = str_replace('en/en/en', 'en', $url);
+//        $url = str_replace('en/en', 'en', $url);
+//        return $url;
     }
 
     public function createAbsoluteUrl($params)
