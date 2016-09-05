20696eb4415e37e9c169da67241ca3956671706b
khaled <kaldmax@hotmail.com>
2016-09-05 05:05:15 -0600

    SIMPLE CODE

--- views/site/about-en.php
+++ views/site/about-en.php
@@ -3,11 +3,18 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'نبذة عن هانج شير';
+$this->title = 'About HangShare';
 ?>
 
 <div class="container">
-    <h1>
-        About HangShare
-    </h1>
+    <h1>About HangShare</h1>
+    <p>Hangshare is created for users who are keen to make an additional income from home. Hangshare helps people make
+        money by utilizing their social pages and traffic sources.</p>
+    <h2>OUR PHILOSOPHY</h2>
+    <p>We are full transparent with the users, as we make money from the advertisements and willing to share this profit
+        with the people who are unable to make this profit by their own.</p>
+    <h2>ESTABLISHMENT</h2>
+    <p>Hangshare is a registedred company established in 2015 in Dubai, United Arab Emirates and has offices in
+        Amman-Jordan.</p>
+    <h3> Post --> Share --> Make Money</h3>
 </div>
