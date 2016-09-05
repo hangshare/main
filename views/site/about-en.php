c7a6e4fd369aa88e2dd56185ce0f3b586646b85b
khaled <kaldmax@hotmail.com>
2016-09-05 05:11:48 -0600

    SIMPLE CODE

--- views/site/about-en.php
+++ views/site/about-en.php
@@ -7,14 +7,18 @@ $this->title = 'About HangShare';
 ?>
 
 <div class="container">
-    <h1>About HangShare</h1>
-    <p>Hangshare is created for users who are keen to make an additional income from home. Hangshare helps people make
-        money by utilizing their social pages and traffic sources.</p>
-    <h2>OUR PHILOSOPHY</h2>
-    <p>We are full transparent with the users, as we make money from the advertisements and willing to share this profit
-        with the people who are unable to make this profit by their own.</p>
-    <h2>ESTABLISHMENT</h2>
-    <p>Hangshare is a registedred company established in 2015 in Dubai, United Arab Emirates and has offices in
-        Amman-Jordan.</p>
-    <h3> Post --> Share --> Make Money</h3>
+    <div class="white-box">
+        <h1>About HangShare</h1>
+        <p>Hangshare is created for users who are keen to make an additional income from home. Hangshare helps people
+            make
+            money by utilizing their social pages and traffic sources.</p>
+        <h2>OUR PHILOSOPHY</h2>
+        <p>We are full transparent with the users, as we make money from the advertisements and willing to share this
+            profit
+            with the people who are unable to make this profit by their own.</p>
+        <h2>ESTABLISHMENT</h2>
+        <p>Hangshare is a registedred company established in 2015 in Dubai, United Arab Emirates and has offices in
+            Amman-Jordan.</p>
+        <h3> Post --> Share --> Make Money</h3>
+    </div>
 </div>
