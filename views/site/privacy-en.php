254d91603445cfafb942d44af2625aeb18c716f3
khaled <kaldmax@hotmail.com>
2016-09-05 05:11:37 -0600

    SIMPLE CODE

--- views/site/privacy-en.php
+++ views/site/privacy-en.php
@@ -2,15 +2,37 @@
 $this->title = 'Hangshare Privacy';
 ?>
 <div class="container">
-    <h1>Hangshare Privacy</h1>
-    <h2>Terms and conditions of use site hangshare:</h2>
-    <p>Terms of use change from time to time, in the case of such changes, we'll display them in this page in order to be aware of them. Your continued use of the hangshare is based on acceptance of the terms and provisions of this use, as amended from time to time in order to achieve better and higher quality service. Any violation of this Agreement by the customer / subscriber, the user will be prohibited to use the website without prior notice and without recovery of any amounts won in some cases.</p>
-    <h2>Protect users</h2>
-    <p>HangShare site management is committed to protect the buyer from fraud or failure to provide better service. In cases of violating the terms and conditions, hangshare has the right to request personal & government documents, plus phone numbers and any information proving the user personality and then have the right to manage the recalculation or continue to prohibit According to the information available. By subscribing to hangshare you agree to govern the website in the kind of problems that occur on a joint account. hangshare sometimes ask for identification documents, to prove the user's identity or proof of ownership of the PayPal account used, and in the case the documents have not been sent to the site, this will keep the account with a tally in the ban. Amounts paid to the customer via PayPal in case of problems is through communication with the technical support of the site's support team through the contact us page, it is the shortest and the best way to ensure your rights.</p>
-    <h2>Use Paypal accounts</h2>
-    <p>To avoid any kind of fraud on Hangshare, payments are not sent to the customer before achieving a minimum of $100 and the payments are to be sent from hangshare within a maximum of 30 days. User's aren't eligible for any winnings or money before achieving $100 in their wallet.</p>
-    <h2>Reasons for account ban:</h2>
-    <p>Hangshare management reserves the right to ban any account when a violation occurs and in this situation, the user loses all the money generated in his/her wallet in Hangshare, also Hangshare will cancel any deposit requests if any .</p>
-    <h2>Banned Topics:</h2>
-    <p>Posts are to be reviewed by Hangshare team before they are published on the website, and Hangshare has the right to delete/ban any post we see unappropriated. In case of violating posts, Hangshare reserves the right to ban the user who posted the escalated post and the user doesn;t have the right to recieve any money generated even if they exceed $100.</p>
+    <div class="white-box">
+        <h1>Hangshare Privacy</h1>
+        <h2>Terms and conditions of use site hangshare:</h2>
+        <p>Terms of use change from time to time, in the case of such changes, we'll display them in this page in order
+            to be aware of them. Your continued use of the hangshare is based on acceptance of the terms and provisions
+            of this use, as amended from time to time in order to achieve better and higher quality service. Any
+            violation of this Agreement by the customer / subscriber, the user will be prohibited to use the website
+            without prior notice and without recovery of any amounts won in some cases.</p>
+        <h2>Protect users</h2>
+        <p>HangShare site management is committed to protect the buyer from fraud or failure to provide better service.
+            In cases of violating the terms and conditions, hangshare has the right to request personal & government
+            documents, plus phone numbers and any information proving the user personality and then have the right to
+            manage the recalculation or continue to prohibit According to the information available. By subscribing to
+            hangshare you agree to govern the website in the kind of problems that occur on a joint account. hangshare
+            sometimes ask for identification documents, to prove the user's identity or proof of ownership of the PayPal
+            account used, and in the case the documents have not been sent to the site, this will keep the account with
+            a tally in the ban. Amounts paid to the customer via PayPal in case of problems is through communication
+            with the technical support of the site's support team through the contact us page, it is the shortest and
+            the best way to ensure your rights.</p>
+        <h2>Use Paypal accounts</h2>
+        <p>To avoid any kind of fraud on Hangshare, payments are not sent to the customer before achieving a minimum of
+            $100 and the payments are to be sent from hangshare within a maximum of 30 days. User's aren't eligible for
+            any winnings or money before achieving $100 in their wallet.</p>
+        <h2>Reasons for account ban:</h2>
+        <p>Hangshare management reserves the right to ban any account when a violation occurs and in this situation, the
+            user loses all the money generated in his/her wallet in Hangshare, also Hangshare will cancel any deposit
+            requests if any .</p>
+        <h2>Banned Topics:</h2>
+        <p>Posts are to be reviewed by Hangshare team before they are published on the website, and Hangshare has the
+            right to delete/ban any post we see unappropriated. In case of violating posts, Hangshare reserves the right
+            to ban the user who posted the escalated post and the user doesn;t have the right to recieve any money
+            generated even if they exceed $100.</p>
+    </div>
 </div>
