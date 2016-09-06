a41c0bd048e93e257b83dfd86ea8a4fe3e559467
khaled <kaldmax@hotmail.com>
2016-09-05 19:21:59 -0600

    SIMPLE CODE

--- views/explore/_innerads.php
+++ views/explore/_innerads.php
@@ -1,12 +1,23 @@
 <li>
-    <script async
-            src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Leaderboard - Post Upper -->
-    <ins class="adsbygoogle"
-         style="display:inline-block;width:728px;height:90px"
-         data-ad-client="ca-pub-6288640194310142"
-         data-ad-slot="1011333310"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+    <?php if (!Yii::$app->helper->isMobile()): ?>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Post List Ad -->
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:970px;height:250px"
+             data-ad-client="ca-pub-6288640194310142"
+             data-ad-slot="1785393319"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    <?php else : ?>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Post List Mobile -->
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:300px;height:250px"
+             data-ad-client="ca-pub-6288640194310142"
+             data-ad-slot="4738859718"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    <?php endif; ?>
 </li>
