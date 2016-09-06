b3352960be83fa953fb4296551e90ba78915af58
khaled <kaldmax@hotmail.com>
2016-09-05 19:55:54 -0600

    SIMPLE CODE

--- views/explore/_innerads.php
+++ views/explore/_innerads.php
@@ -1,11 +1,11 @@
 <li>
     <?php if (!Yii::$app->helper->isMobile()): ?>
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- Post List Ad -->
+        <!-- Post List Leaderboard -->
         <ins class="adsbygoogle"
-             style="display:inline-block;width:970px;height:250px"
+             style="display:inline-block;width:728px;height:90px"
              data-ad-client="ca-pub-6288640194310142"
-             data-ad-slot="1785393319"></ins>
+             data-ad-slot="8272578919"></ins>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
