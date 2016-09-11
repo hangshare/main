a27868e10608dce5e2cc6b0cfd4fbbe3e73375fd
khaled <kaldmax@hotmail.com>
2016-09-11 06:15:30 +0300

    SIMPLE CODE

--- views/explore/_commentform.php
+++ views/explore/_commentform.php
@@ -1,6 +1,11 @@
 <?php if (!Yii::$app->user->isGuest) : ?>
     <?php $imSm = Yii::$app->imageresize->thump(Yii::$app->user->identity->image, 50, 50, 'crop'); ?>
-    <div style="border: 1px solid #eee; padding: 15px;">
+    <div style="border-radius: 3px;  margin-bottom: 0;
+    margin-top: 20px; background: #fff none repeat scroll 0 0;
+    border: 1px solid rgba(0, 0, 0, 0.09);
+    border-radius: 3px;
+    padding: 20px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.04);">
         <img width="25" src="<?= $imSm; ?>"> <span><?= Yii::$app->user->identity->name; ?></span>
         <form action="#">
             <textarea date-id="<?= $id ?>" placeholder="<?= Yii::t('app', 'Write a comment ...') ?>"
