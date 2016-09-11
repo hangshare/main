01121a2c7e320638ef16af7efd2fc742de21450c
khaled <kaldmax@hotmail.com>
2016-09-11 04:13:11 +0300

    SIMPLE CODE

--- views/explore/index.php
+++ views/explore/index.php
@@ -47,9 +47,11 @@ if (empty($this->title)) {
         <?php endif ?>
     </div>
     <div class="col-md-9">
-        <div class="row">
-            <h1 class="normal"><?= $cat->title ?></h1>
-        </div>
+        <?php if (isset($cat)) : //bug ?>
+            <div class="row">
+                <h1 class="normal"><?= $cat->title ?></h1>
+            </div>
+        <?php endif; ?>
         <hr class="nomargin m-b-20">
         <div class="row">
             <div class="col-md-12">
