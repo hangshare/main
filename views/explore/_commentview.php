81a7033b94ed424091b42e98be392bc1f6084ee8
khaled <kaldmax@hotmail.com>
2016-09-14 17:21:13 +0300

    SIMPLE CODE

--- views/explore/_commentview.php
+++ views/explore/_commentview.php
@@ -2,7 +2,9 @@
 use yii\helpers\Html;
 
 $thump = Yii::$app->imageresize->thump($model->user->image, 80, 80, 'crop');
+$username = empty($model->username) ? $model->id : $model->username;
 ?>
+
 <li style="border-radius: 3px;  margin-bottom: 0;
     margin-top: 20px; background: #fff none repeat scroll 0 0;
     border: 1px solid rgba(0, 0, 0, 0.09);
@@ -11,12 +13,12 @@ $thump = Yii::$app->imageresize->thump($model->user->image, 80, 80, 'crop');
     box-shadow: 0 1px 4px rgba(0, 0, 0, 0.04);">
     <div style="margin-bottom: 15px;">
         <div style='display: inline-block; vertical-align: top;'>
-            <a href="#">
+            <a href="<?= Yii::$app->urlManager->createUrl(['//user/view', 'id' => $username]) ?>">
                 <?= Html::img($thump, ['class' => '', 'style' => 'border-radius: 100%;display: inline-block;vertical-align: middle;', 'width' => 40]); ?>
             </a>
         </div>
         <div style="display: inline-block; vertical-align: top; font-size: 14px; ">
-            <a style="display: block;"><?= Html::encode($model->user->name) ?></a>
+            <a href="<?= Yii::$app->urlManager->createUrl(['//user/view', 'id' => $username]) ?>" style="display: block;"><?= Html::encode($model->user->name) ?></a>
             <?= date('M d', strtotime($model->created_at)); ?>
         </div>
     </div>
