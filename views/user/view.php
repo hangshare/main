902df1e3162df7f04543409c6dd03bba45548b8d
khaled <kaldmax@hotmail.com>
2016-09-18 15:52:26 +0300

    SIMPLE CODE

--- views/user/view.php
+++ views/user/view.php
@@ -133,3 +133,14 @@ $thump = Yii::$app->imageresize->thump($model->image, 80, 80, 'crop');
         </div>
     </div>
 </div>
+
+<!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+    window.$zopim||(function(d,s){var z=$zopim=function(c){
+        z._.push(c)},$=z.s=
+        d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+        $.src='//v2.zopim.com/?4Djyp0FocsEjBAeTYUm1jZQhm9JE1bH3';z.t=+new Date;$.
+            type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+</script>
+<!--End of Zopim Live Chat Script-->
