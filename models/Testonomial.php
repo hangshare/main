ead0de2928fbba2144284a569cd56d13eb57c511
khaled <kaldmax@hotmail.com>
2016-09-29 00:52:44 +0300

    SIMPLE CODE

---
+++ models/Testonomial.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user_settings".
+ *
+ * @property integer $userId
+ * @property string $userTitle
+ * @property string $bodyText
+ * @property string $lang
+ *
+ * @property User $user
+ */
+class Testonomial extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'testonomial';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['userId'], 'integer'],
+            [['lang'], 'string', 'max' => 2],
+            [['userTitle'], 'string', 'max' => 200],
+            [['bodyText'], 'string', 'max' => 1000],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'userId' => Yii::t('app', 'User ID'),
+            'bodyText' => Yii::t('app', 'Body Text'),
+            'userTitle' => Yii::t('app', 'User Title')
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'userId']);
+    }
+}
