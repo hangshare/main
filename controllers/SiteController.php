3f4ac5ce021770466fb2efd5e72f6b3273fe43fa
khaled <kaldmax@hotmail.com>
2016-11-08 02:28:33 +0200

    SIMPLE CODE

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -92,8 +92,8 @@ class SiteController extends Controller
                 mkdir(Yii::$app->basePath . '/media/user', 0777, true);
             }
             file_put_contents($imageFile, $imagecontent);
-            Yii::$app->customs3->uploadFromPath($imageFile, 'hangshare-media', 'fa/' . $user->image);
-            Yii::$app->imageresize->PatchResize('hangshare-media', 'fa/' . $user->image, 'user');
+            Yii::$app->customs3->uploadFromPath($imageFile, 'hangshare-media', $user->image);
+            Yii::$app->imageresize->PatchResize('hangshare-media', $user->image, 'user');
             echo $user->id . '<br>';
             $user->save(false);
         }
