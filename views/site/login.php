82e1b438c1843a1195181f199214f7d544d57859
khaled <kaldmax@hotmail.com>
2016-11-08 17:46:27 +0200

    SIMPLE CODE

--- views/site/login.php
+++ views/site/login.php
@@ -2,27 +2,10 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
-
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-
 $this->title = Yii::t('app', 'Sign in to your account');
 $this->params['breadcrumbs'][] = $this->title;
-
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-$fb = new Facebook\Facebook([
-    'app_id' => '1024611190883720',
-    'app_secret' => '0df74c464dc8e58424481fb4cb3bb13c',
-    'default_graph_version' => 'v2.4',
-    'persistent_data_handler' => 'session'
-]);
-$helper = $fb->getRedirectLoginHelper();
-
-$params = ['scope' => 'email,user_about_me'];
-$fUrl = $helper->getLoginUrl('https://www.hangshare.com/site/facebook/', $params);
 ?>
 <div class="container">
     <div class="white-box row">
@@ -31,13 +14,12 @@ $fUrl = $helper->getLoginUrl('https://www.hangshare.com/site/facebook/', $params
                 <div class="row">
                     <h1 class="font-18"><?= Html::encode($this->title) ?></h1>
                     <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                    <?= $form->field($model, 'username') ?>
+                    <?= $form->field($model, 'username')->input('email') ?>
                     <?= $form->field($model, 'password')->passwordInput() ?>
                     <?= $form->field($model, 'rememberMe')->checkbox() ?>
                     <div class="pull-right">
                         <?= Html::a(Yii::t('app','Forget your username or password?'), ['//request-password-reset']) ?>
                     </div>
-
                     <div class="form-group">
                         <?=
                         Html::submitButton(Yii::t('app', 'Login'), [
@@ -66,7 +48,7 @@ $fUrl = $helper->getLoginUrl('https://www.hangshare.com/site/facebook/', $params
                             eventCategory: 'Login',
                             eventAction: 'Facebook',
                             eventLabel: 'Login Page'
-                        });" href="<?= $fUrl; ?>" class="btn btn-primary"
+                        });" href="javascript:void(0);" rel="nofollow" class="btn btn-primary fb-login"
                        style="background-color: #3b5998;">
                         <i class="fa fa-fw fa-facebook pull-left fb-icon"></i>
                         <span class="pull-left"><?= Yii::t('app', 'Signin using your Facebook account') ?></span>
