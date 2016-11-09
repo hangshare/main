98be2b5856d131c0b27af2ed2e8c892d24d15eee
khaled <kaldmax@hotmail.com>
2016-11-10 01:02:38 +0200

    SIMPLE CODE

--- views/explore/post.php
+++ views/explore/post.php
@@ -5,34 +5,121 @@ use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use app\models\Category;
-
 /* @var $this yii\web\View */
 /* @var $model app\models\Post */
 /* @var $form yii\widgets\ActiveForm */
-$this->title = Yii::t('app', 'New post');
+$this->title = 'موضوع جديد';
 $thump = Yii::$app->imageresize->thump($model->cover, 150, 100, 'crop');
 foreach ($model->postBodies as $data) {
     $model->body .= $data->body;
 }
 ?>
-<?php
-$form = ActiveForm::begin(['options' => [
-    'id' => 'add-post'
-    , 'enctype' => 'multipart/form-data']])
-?>
-<div class="row">
-    <div class="col-md-12 res-nopadding">
-        <div class="col-md-9" style="background-color: #fff;padding-top: 50px;  min-height: 700px;">
-            <div class="container res-nopadding">
-                <?= $form->field($model, 'title')->textArea([
-                    'maxlength' => 80,
-                    'style' => '  border: medium none;box-shadow: none;font-size: 40px;height: 140px;margin-bottom: 20px;padding: 0;resize: none;',
-                    'placeholder' => Yii::t('app', 'Add Title')
-                ])->label(false) ?>
-                <div class="editable"></div>
-                <div class="form-group m-t-25">
-                    <?php
-                    $event = $model->isNewRecord ? "ga('send', {
+<div class="container m-t-25">
+    <div class="post-form col-md-9">
+        <div class="white pad">
+            <h3><?= Yii::t('app', 'post info') ?></h3>
+            <hr/>
+            <?php
+            $form = ActiveForm::begin(['options' => [
+                'id' => 'add-post'
+                , 'enctype' => 'multipart/form-data']])
+            ?>
+            <div id="cover_error" style="display: none;">
+                <span class="alert alert-danger" style="display: block; text-align: center;">
+            <?= Yii::t('app', 'please add cover image') ?>
+            </span>
+            </div>
+            <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+            <label><?= Yii::t('app', 'cover image') ?></label>
+
+            <div class="row">
+                <div class="col-md-2">
+                    <?php echo Html::img($thump, ['class' => 'img-responsive', 'id' => 'coveri']); ?>
+                    <input id="covercheck" type="hidden" name="covercheck"
+                           value="<?= empty($model->cover) ? '' : '0' ?>">
+
+                    <div id="prev"
+                         style="background-color: rgba(0, 0, 0, 0.4);padding: 40px 38px;position: absolute;text-align: center;top: 0; display: none;">
+                        <i class="fa fa-spin fa-spinner fa-2x" style="position: relative; top: -10px; color: #fff;"></i>
+                    </div>
+                    <button class="btn btn-primary btn-block" id="uploadtos3" style="border-radius: 0;">
+                        <span><?= Yii::t('app', 'Choose a picture') ?></span>
+                    </button>
+                </div>
+                <!--                <div class="col-md-9">-->
+                <?php //echo $form->field($model, 'ylink')->textInput(['maxlength' => true, 'placeholder' => Yii::t('app', 'youtube ex')]) ?>
+                <!--                </div>-->
+            </div>
+            <input id="cover_input" name="cover" type="hidden" value=""/>
+            <br>
+            <?php
+            $arr = [];
+            foreach ($model->postCategories as $cat_selected) {
+                $arr[] = $cat_selected->categoryId;
+            }
+            $model->categories = $arr;
+            ?>
+            <label for="post-categories" class="control-label"><?= Yii::t('app', 'Categories') ?></label>
+            <?php
+            $menu = Category::find()
+                ->where("lang = '" . Yii::$app->language . "'")
+                ->all();
+            $mainMenu = [];
+            $i = 2;
+            foreach ($menu as $menuData) {
+                if ($menuData->parent) {
+                    $mainMenu[$menuData->parent][$menuData->id] = $menuData->title;
+                }
+            }
+            $catData = [];
+            foreach ($menu as $menuData) {
+                if (!$menuData->parent) {
+                    $catData[$menuData->title] = $mainMenu[$menuData->id];
+                }
+            }
+            echo Select2::widget([
+                'name' => 'Post[categories]',
+                'value' => $model->categories,
+                'data' => $catData,
+                'options' => ['multiple' => true, 'placeholder' => Yii::t('app', 'Categories')],
+                'pluginOptions' => [
+                    'tags' => false,
+                ],
+            ]);
+            ?>
+            <br>
+            <?php
+            $tags = [];
+            foreach ($model->postTags as $post_tag) {
+                $tags[] = $post_tag->tag;
+            }
+            $tags_string = implode(',',$tags);
+            ?>
+            <?= $form->field($model, 'body')->textarea(['class' => 'froala-edit']) ?>
+            <label><?= Yii::t('app', 'Tags') ?></label>
+            <?php
+            $tags_sql = '';
+            if($tags_string){
+                $tags_sql = "OR id IN ({$tags_string})";
+            }
+            echo Select2::widget([
+                'name' => 'Post[keywords]',
+                'value' => $tags,
+                'data' => ArrayHelper::map(Tags::find()
+                    ->where("(published = 1 AND lang = '" . Yii::$app->language . "' ) $tags_sql")
+                    ->select('id, name')
+                    ->orderBy('name')
+                    ->all(), 'id', 'name'),
+                'options' => ['multiple' => true, 'placeholder' => Yii::t('app', 'add tags')],
+                'pluginOptions' => [
+                    'tags' => true,
+                    'maximumInputLength' => 30
+                ],
+            ]);
+            ?>
+            <div class="form-group m-t-25">
+                <?php
+                $event = $model->isNewRecord ? "ga('send', {
                             hitType: 'event',
                             eventCategory: 'Post',
                             eventAction: 'Add',
@@ -43,119 +130,41 @@ $form = ActiveForm::begin(['options' => [
                             eventAction: 'Edit',
                             eventLabel: 'Update Post'
                         });";
-                    ?>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 res-m-t55">
-            <div class="white-box text-center">
-                <p class="text-center"><?= Yii::t('app', 'Post Quality') ?></p>
-                <h2 id="postq" class="text-center" style="font-size: 80px;">0%</h2>
+                ?>
                 <?=
                 Html::submitButton($model->isNewRecord ? Yii::t('app', 'Post') : Yii::t('app', 'Save'), [
                     'id' => 'main-post',
-                    'class' => 'btn btn-primary btn-block',
+                    'class' => 'btn btn-primary pull-left',
                     'onclick' => $event
                 ])
                 ?>
             </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+    <div class="col-md-3 res-m-t55">
+        <div class="row">
+            <div class="white-box" style="border: 1px solid #51B415;">
+                <h3 style="margin-top: 10px;"><?= Yii::t('app', 'Note') ?> : </h3>
 
+                <p><?= Yii::t('app', 'Add-Post-Note-1') ?></p>
 
-            <?php
-            $checked = [];
-            foreach ($model->postCategories as $cat_selected) {
-                $checked[] = $cat_selected->categoryId;
-            }
-            ?>
-            <?php
-            $mainMenu = Yii::$app->cache->get('media-data-' . Yii::$app->language);
-            if ($mainMenu == false) {
-                $menu = Category::find()->where(['lang' => Yii::$app->language])->all();
-                $articlesurl = Yii::t('app', 'articles-url');
-                $mainMenu = [];
-                foreach ($menu as $menuData) {
-                    if ($menuData->parent) {
-                        $mainMenu[$menuData->parent]['sub'][] = ['id' => $menuData->id, 'title' => $menuData->title, 'url' => $menuData->url_link];
-                    } else {
-                        $mainMenu[$menuData->id] = ['id' => $menuData->id,
-                            'title' => $menuData->title,
-                            'url' => $menuData->url_link];
-                    }
-                }
-                Yii::$app->cache->set($mainMenu, 'media-data-' . Yii::$app->language, 3600);
-            }
-            ?>
-            <h3><?= Yii::t('app', 'Categories') ?></h3>
-            <div class="padding0" style="background-color: #fff;">
-                <div class="tabs-panel">
-                    <ul class="categorychecklist form-no-clear list-unstyled">
-                        <?php foreach ($mainMenu as $mData) : ?>
-                            <li class="popular-category wpseo-term-unchecked">
-                                <label class="selectit">
-                                    <input class="checkcat"
-                                           data-id="category"
-                                           type="checkbox" <?php if (in_array($mData['id'], $checked)) echo 'checked="checked"' ?>
-                                           name="post_category[]"
-                                           value="<?= $mData['id'] ?>">
-                                    <?= $mData['title'] ?>
-                                </label>
-                                <?php if (isset($mData['sub'])): ?>
-                                    <ul class="popular-category wpseo-term-unchecked">
-                                        <?php foreach ($mData['sub'] as $submenu) : ?>
-                                            <li class="popular-category wpseo-term-unchecked">
-                                                <label class="selectit">
-                                                    <input class="checkcat"
-                                                           data-id="category"
-                                                           type="checkbox" <?php if (in_array($submenu['id'], $checked)) echo 'checked="checked"' ?>
-                                                           name="post_category[]"
-                                                           value="<?= $submenu['id'] ?>">
-                                                    <?= $submenu['title'] ?>
-                                                </label>
-                                            </li>
-                                        <?php endforeach; ?>
-                                    </ul>
-                                <?php endif; ?>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
             </div>
-
-
-            <?php
-            $tags = [];
-            foreach ($model->postTags as $post_tag) {
-                $tags[] = $post_tag->tag;
-            }
-            $tags_string = implode(',', $tags);
-            ?>
-
-            <label><?= Yii::t('app', 'Tags') ?></label>
-            <?php
-            $tags_sql = '';
-            if ($tags_string) {
-                $tags_sql = "OR id IN ({$tags_string})";
-            }
-                        echo Select2::widget([
-                            'name' => 'Post[keywords]',
-                            'value' => $tags,
-                            'data' => ArrayHelper::map(Tags::find()
-                                ->where("(published = 1 AND lang = '" . Yii::$app->language . "' ) $tags_sql")
-                                ->select('id, name')
-                                ->orderBy('name')
-                                ->all(), 'id', 'name'),
-                            'options' => ['multiple' => true, 'placeholder' => Yii::t('app', 'add tags')],
-                            'pluginOptions' => [
-                                'tags' => true,
-                                'maximumInputLength' => 30
-                            ],
-                        ]);
-            ?>
-
+        </div>
+        <div class="row m-t-25">
+            <div class="white-box">
+                <h3 style="margin-top: 10px;"><?= Yii::t('app', 'Tips') ?>: </h3>
+                <?php if (Yii::$app->language == 'en') {
+                    echo $this->render('tips-en');
+                } else {
+                    echo $this->render('tips-ar');
+                }
+                ?>
+            </div>
         </div>
     </div>
 </div>
-<?php ActiveForm::end(); ?>
+
 <from id="uploadform" method="POST" enctype="multipart/form-data" style="display: none;">
     <input id="files3" type="file"/>
     <input id="type" value="post"/>
