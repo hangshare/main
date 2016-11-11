8c5b447e36804e3b0a1d196f736967839e3b51aa
khaled <kaldmax@hotmail.com>
2016-11-11 18:30:54 +0200

    SIMPLE CODE

--- web/index.php
+++ web/index.php
@@ -1,14 +1,14 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 
 
 
-//error_reporting(0);
-//ini_set('display_errors', 0);
+error_reporting(0);
+ini_set('display_errors', 0);
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
 
 
 require(__DIR__ . '/../vendor/autoload.php');
