2ec770211d41189181487e063deaa9b211540e96
khaled <kaldmax@hotmail.com>
2016-11-29 05:22:24 +0200

    SIMPLE CODE

--- views/layouts/htmlhead.php
+++ views/layouts/htmlhead.php
@@ -97,7 +97,7 @@ $canonical = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
         <!-- End Facebook Pixel Code -->
     <?php endif; ?>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
 </head>
 <body>
 <?php foreach (Yii::$app->session->getAllFlashes() as $message):; ?>
