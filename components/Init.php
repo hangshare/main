6832aa3f7e4c9ede6996fd00f99ead2357db65ad
khaled <kaldmax@hotmail.com>
2016-11-29 03:19:35 +0200

    SIMPLE CODE

--- components/Init.php
+++ components/Init.php
@@ -80,7 +80,7 @@ class Init extends Component
 
     protected function language()
     {
-        if (strpos(Yii::$app->request->url, 'en/') === false && isset($_COOKIE['userlanghangshare']) && $_COOKIE['userlanghangshare'] == 'en' && $this->isHome()) {
+        if (strpos(Yii::$app->request->url, 'en/') === false && isset($_COOKIE['userlanghangshare']) && $_COOKIE['userlanghangshare'] == 'en') {
             header("Location: https://www.hangshare.com/en/");
             exit(0);
         }
