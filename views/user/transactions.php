8b6d62ce57effe01f6ee51fcf9a13adcfcb5cae0
khaled <kaldmax@hotmail.com>
2016-12-05 15:51:48 +0200

    SIMPLE CODE

---
+++ views/user/transactions.php
@@ -0,0 +1,35 @@
+<?php
+use yii\bootstrap\Html;
+use yii\widgets\ListView;
+
+$this->title = Yii::t('app', 'Money Report');
+?>
+<div class="container">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>#<?= Yii::t('app', 'ID') ?></th>
+            <th><?= Yii::t('app', 'Receipt'); ?></th>
+            <th><?= Yii::t('app', 'Amount'); ?></th>
+            <th><?= Yii::t('app', 'Status'); ?></th>
+            <th><?= Yii::t('app', 'Date'); ?></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?=
+        ListView::widget([
+            'dataProvider' => $dataProvider,
+            'itemView' => '_transactions',
+            'layout' => "<ul class='list-unstyled'>{items}\n</ul>{pager}",
+            'options' => [],
+            'itemOptions' => [
+                'tag' => false,
+            ]
+        ]);
+        ?>
+        </tbody>
+    </table>
+</div>
