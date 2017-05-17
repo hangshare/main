57a2388360cdd8c0eb6d77528811d257074e4244
khaled <kaldmax@hotmail.com>
2017-05-17 18:43:00 +0300

    SIMPLE CODE

---
+++ components/WordValidator.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khaled
+ * Date: 5/17/2017
+ * Time: 5:31 PM
+ */
+
+namespace app\components;
+
+use Yii;
+use yii\validators\Validator;
+
+
+class WordValidator extends Validator
+{
+    public function validateAttribute($model, $attribute)
+    {
+        $total_words = str_word_count($model->$attribute);
+
+        if ($total_words > 20) {
+            $this->addError($model, $attribute, Yii::t('app', 'wordsvalidations-max'));
+        }
+        if ($total_words < 5) {
+            $this->addError($model, $attribute, Yii::t('app', 'wordsvalidations-min'));
+        }
+    }
+}
