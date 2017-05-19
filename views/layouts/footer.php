4195540ae9b2cd773d323c57e33b11d37eaa08c1
khaled <kaldmax@hotmail.com>
2017-05-19 04:38:02 +0300

    SIMPLE CODE

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -39,5 +39,9 @@ use yii\helpers\Html;
     </div>
 </footer>
 <?php $this->endBody() ?>
+<?php if (Yii::$app->controller->action->id == 'view'): ?>
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-591e3640803701e5"></script>
+<?php endif; ?>
 </body>
 </html>
