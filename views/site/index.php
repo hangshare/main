ba2ef144f1a5ece8c813696db85fcf62d39b6a3a
khaled <kaldmax@hotmail.com>
2017-05-20 02:30:55 +0300

    SIMPLE CODE

--- views/site/index.php
+++ views/site/index.php
@@ -173,7 +173,7 @@ $articlesurl = Yii::t('app', 'articles-url');
                                 <div class="testimonial_nav_info text-center">
                                     <h5 class="text-center"><?= $testonimoial->user->name ?></h5>
                                     <span
-                                        style="font-style:italic; font-size:15px;"><?= $testonimoial->userTitle ?></span>
+                                            style="font-style:italic; font-size:15px;"><?= $testonimoial->userTitle ?></span>
                                 </div>
                             </div>
                         </div>
@@ -205,85 +205,6 @@ $articlesurl = Yii::t('app', 'articles-url');
                 <div class="padding-top-60 text-center">
                     <a class="btn btn-default btn-lg"
                        href="<?= Yii::$app->urlManager->createUrl(["//{$articlesurl}"]) ?>"><?= Yii::t('app', 'go to the blog') ?></a>
-                </div>
-            </div>
-        </section>
-        <hr>
-        <section id="pricing-section" class="pricing-section section"
-                 style="background-color: #f5f5f5; padding-bottom: 80px">
-            <div class="container">
-                <div class="section-title">
-                    <h3 class="section-title-heading text-center"><?= Yii::t('app', 'Register') ?>
-                        <span><?= Yii::t('app', 'Now') ?></span></h3>
-                </div>
-                <div class="row padding-top-20">
-                    <div class="col-sm-4 col-sm-offset-2 margin-bottom-xs-40 wow fadeInUpLeftScale animated">
-                        <div class="pricing-table">
-                            <div class="pricing-header font-second golden">
-                                <h3 class="text-center"><?= Yii::t('app', 'Gold Account') ?></h3>
-                                <div class="price">
-                                    <span class="currency">$</span>
-                                    <span class="value">10</span>
-                                    <span class="duration"><?= Yii::t('app', 'mo') ?></span>
-                                </div>
-                            </div>
-                            <!--/ .pricing-header -->
-                            <div class="pricing-body">
-                                <ul class="pricing-features">
-                                    <li><?= Yii::t('app', 'goldtips1') ?></li>
-                                    <li><?= Yii::t('app', 'goldtips2') ?></li>
-                                    <li><?= Yii::t('app', 'goldtips3') ?></li>
-                                    <li><?= Yii::t('app', 'goldtips4') ?></li>
-                                    <li><?= Yii::t('app', 'goldtips5') ?></li>
-                                    <li><?= Yii::t('app', 'goldtips6') ?></li>
-                                </ul>
-                            </div>
-                            <div class="pricing-footer">
-                                <?=
-                                Html::a(Yii::t('app', 'Go Premium'), [Yii::$app->user->isGuest ? '//register/' : 'site/plangold'], [
-                                    'class' => 'btn btn-default',
-                                    'style' => 'background-color: #ffd700;border: 0 none;border-radius: 0;color: #fff;text-transform: uppercase;',
-                                    'rel' => 'nofollow',
-                                    'data' => [
-                                        'method' => 'post',
-                                        'params' => ['plan' => 'b'],
-                                    ]
-                                ]);
-                                ?>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="popular col-sm-4 margin-bottom-xs-40 wow fadeInUpScale animated">
-                        <div class="pricing-table">
-                            <div class="pricing-header font-second">
-                                <h3 class="text-center"><?= Yii::t('app', 'Popular') ?></h3>
-                                <div class="price">
-                                    <span style="line-height: 50px;"><?= Yii::t('app', 'Free Account') ?></span>
-                                </div>
-                            </div>
-                            <div class="pricing-body">
-                                <ul class="pricing-features">
-                                    <li><?= Yii::t('app', 'freetips1') ?></li>
-                                    <li><?= Yii::t('app', 'freetips2') ?></li>
-                                    <li class="nodecoration"> -</li>
-                                    <li class="nodecoration"> -</li>
-                                    <li class="nodecoration"> -</li>
-                                    <li><?= Yii::t('app', 'freetips3') ?></li>
-                                </ul>
-                            </div>
-                            <div class="pricing-footer">
-                                <?= Html::a(Yii::t('app', 'Free Registrations'), ['//register/'], [
-                                    'class' => 'btn btn-default btn-block',
-                                    'style' => 'color:#fff;background-color:#757a86 ;border: 0 none;border-radius: 0;text-transform: uppercase;',
-                                    'data' => [
-                                        'method' => 'post',
-                                        'params' => ['plan' => 'a'],
-                                    ]
-                                ])
-                                ?>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </section>
