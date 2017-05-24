67eb03d969e985f460e69b1772bd2b6ae67564ce
khaled <kaldmax@hotmail.com>
2017-05-24 05:13:37 +0300

    SIMPLE CODE

--- models/Post.php
+++ models/Post.php
@@ -180,7 +180,7 @@ class Post extends \yii\db\ActiveRecord
             if (!empty($json)) {
 
                 $json = json_decode($json);
-                var_dump($json);die();
+
                 $this->cover = json_encode([
                     'type' => 's3',
                     'image' => $json->key,
@@ -188,6 +188,8 @@ class Post extends \yii\db\ActiveRecord
                     'width' => $json->width,
                     'height' => $json->height
                 ]);
+
+                var_dump($this->cover);die();
             }
         }
     }
