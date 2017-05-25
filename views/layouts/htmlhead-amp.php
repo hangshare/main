6f772b0420d1f04d35083fcadefa3ca78ba85d3a
khaled <kaldmax@hotmail.com>
2017-05-25 05:47:19 +0300

    SIMPLE CODE

--- views/layouts/htmlhead-amp.php
+++ views/layouts/htmlhead-amp.php
@@ -6,21 +6,31 @@ use app\assets\AppAsset;
 $host = $_SERVER['HTTP_HOST'];
 $uri = $_SERVER['REQUEST_URI'];
 
-$canonical = str_replace('amp/','',"https://{$host}{$uri}");
+$canonical = str_replace('amp/', '', "https://{$host}{$uri}");
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html amp  lang="<?= Yii::$app->language ?>">
+<html amp lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
-    <link rel="canonical" href="__CANONICAL_URL__" >
+    <link rel="canonical" href="__CANONICAL_URL__">
     <meta name="viewport"
           content="width=device-width,minimum-scale=1,initial-scale=1">
-    <style>body {opacity: 0}</style>
+    <style>body {
+            opacity: 0
+        }</style>
     <noscript>
-        <style>body {opacity: 1}</style>
+        <style>body {
+                opacity: 1
+            }</style>
     </noscript>
     <style amp-custom>
+        <?php if(Yii::$app->language == 'ar') : ?>
+        h1,h2,h3,h4,h5,o,a,span{
+            text-align: right;
+            direction: rtl;
+        }
+        <?php endif; ?>
     </style>
     <script async src="https://cdn.ampproject.org/v0.js"></script>
     <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
