125a3f666eaefa62393d64b83a465ced0f63235e
khaled <kaldmax@hotmail.com>
2017-05-25 05:53:18 +0300

    SIMPLE CODE

--- views/layouts/htmlhead-amp.php
+++ views/layouts/htmlhead-amp.php
@@ -31,6 +31,7 @@ $canonical = str_replace('amp/', '', "https://{$host}{$uri}");
             direction: rtl;
         }
         <?php endif; ?>
+        p,a,span{font-size: 16px}
     </style>
     <script async src="https://cdn.ampproject.org/v0.js"></script>
     <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
